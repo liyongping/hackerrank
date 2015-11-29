97855127d07c13662e2a5b576a5e537cce96cbed
vincent <lypkiller@gmail.com>
2015-11-29 22:44:10 +0800

    add template

---
+++ c.tpl.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("%s\n", "Hello World");
+    return 0;
+}
---
+++ cpp.tpl.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
