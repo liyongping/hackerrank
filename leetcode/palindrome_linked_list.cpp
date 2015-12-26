a89301ff303906927f17a18acd0a2c187958f93e
vincent <lypkiller@gmail.com>
2015-12-26 23:20:15 +0800

    palindrome linked list, with a recursive solution

--- leetcode/palindrome_linked_list.cpp
+++ leetcode/palindrome_linked_list.cpp
@@ -25,8 +25,57 @@ struct ListNode {
 
 class Solution {
 public:
+    ListNode* reverseList(ListNode* head) {
+
+        ListNode *pre= NULL, *curr=head;
+        ListNode * saveNext = NULL;
+        while(curr){
+            saveNext = curr->next;
+            curr->next = pre;
+            pre = curr;
+            curr = saveNext;
+        }
+
+        return pre;
+    }
+
     bool isPalindrome(ListNode* head) {
-        
+        ListNode dummy(-1);
+        dummy.next = head;
+        ListNode *fast = &dummy, *slow = &dummy;
+
+        // find mid node by slow and fast pointer
+        while(fast && fast->next){
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+
+        //slow->next will be the head of second list.
+        //the length of slow > the length of fast (+1)
+        ListNode *p2 = reverseList(slow->next);
+
+        ListNode* p1 = dummy.next;
+        while(p2){
+            if(p1->val != p2->val)
+                return false;
+            p1 = p1->next;
+            p2 = p2->next;
+        }
+        return true;
+    }
+
+    // recursive
+    ListNode* temp;
+    bool isPalindrome2(ListNode* head) {
+        temp = head;
+        return check(head);
+    }
+
+    bool check(ListNode* p) {
+        if (NULL == p) return true;
+        bool isPal = check(p->next) & (temp->val == p->val);
+        temp = temp->next;
+        return isPal;
     }
 };
 
@@ -48,13 +97,13 @@ int main(int argc, char const *argv[])
 
     ListNode *head = new ListNode(0); 
     ListNode *pcurr = head;
-    for(int i=1; i< 9; i++){
+    for(int i=1; i< 1; i++){
         ListNode *node = new ListNode(i);
         pcurr->next = node;
         pcurr = pcurr->next;
     }
 
-    for(int i=9; i >= 0; i--){
+    for(int i=-1; i >= 0; i--){
         ListNode *node = new ListNode(i);
         pcurr->next = node;
         pcurr = pcurr->next;
