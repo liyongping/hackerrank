3979aa8d756d6b17760b891793c7da69530fee5c
yongping.li <lypkiller@gmail.com>
2016-08-25 22:21:15 +0800

    add a help function printVectorInt

--- cpp.tpl.cpp
+++ cpp.tpl.cpp
@@ -3,10 +3,21 @@
 #include <vector>
 
 using namespace std;
+void printVectorInt(vector<int> & vs);
+
 
 
 int main(int argc, char const *argv[])
 {
     /* code */
     return 0;
+}
+
+
+void printVectorInt(vector<int> & vs){
+    for (int i = 0; i < vs.size(); ++i)
+    {
+        cout<< vs[i] << " ";
+    }
+    cout<< endl;
 }
