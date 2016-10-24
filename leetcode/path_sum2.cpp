6b7453d7a236d35a63a2ef94784820fbc111fc43
yongping.li <lypkiller@gmail.com>
2016-10-24 18:30:27 +0800

    formate the comments

--- leetcode/path_sum2.cpp
+++ leetcode/path_sum2.cpp
@@ -5,7 +5,8 @@
 using namespace std;
 
 /*
- Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+ Given a binary tree and a sum, 
+ find all root-to-leaf paths where each path's sum equals the given sum.
 For example:
 Given the below binary tree and sum = 22,
 
